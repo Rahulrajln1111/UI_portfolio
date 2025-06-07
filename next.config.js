cb622c1a2e07d5b4f92de8efe5de122e71711c8d
Rahul Razz <razz@Razz.Razz>
2025-06-07 22:42:24 +0530

    updating package.json and netlify

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',             // Enables static export
+  output: 'export',
   images: {
-    unoptimized: true,          // Disables Image Optimization for static export
+    unoptimized: true,
   },
-  // Uncomment and edit if you want custom routing
-  // exportPathMap: async () => ({
-  //   '/': { page: '/' },
-  //   '/about': { page: '/about' },
-  //   '/contact': { page: '/contact' },
-  //   // Add other routes here
-  // }),
 };
 
 module.exports = nextConfig;
